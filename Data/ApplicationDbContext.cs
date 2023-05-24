6b90b5a3862a23fb3b825787c8b1532c07975f08
Jackson Thurn <jackson.thurn@ndsu.edu>
2023-05-24 17:46:14 +0000

    Section 4 10

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Training_LeaveManagementWeb.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<Employee>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
