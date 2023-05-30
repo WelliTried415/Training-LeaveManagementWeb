80d1a645267c49218bb5a606c665c2dfe88e6205
Jackson Thurn <jackson.thurn@ndsu.edu>
2023-05-30 15:28:41 -0500

    modified create, edit, and details views for LeaveType

---
+++ Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Training_LeaveManagementWeb.Data;
+using Training_LeaveManagementWeb.Models;
+
+namespace Training_LeaveManagementWeb.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig() 
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
